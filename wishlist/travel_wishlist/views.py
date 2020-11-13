5b6a0eb36ef032392f2d673c0eb7796106d16ce7
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-13 00:03:56 -0600

    views.py updated with login requirement.

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm 
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def place_list(request):
 
     """ If this is a POST request, the user clicked the Add button
@@ -27,12 +29,12 @@ def place_list(request):
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', { 'places': places, 'new_place_form': new_place_form })
 
-
+@login_required
 def places_visited(request):
     visited = Place.objects.filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', { 'visited': visited })
 
-
+@login_required
 def place_was_visited(request, place_pk):
     if request.method == 'POST':
         place = get_object_or_404(Place, pk=place_pk)
@@ -40,6 +42,3 @@ def place_was_visited(request, place_pk):
         place.save()
     
     return redirect('place_list')
-
-
-    
