a6ac324912be185b462a7de2502c59bfbb879874
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-13 23:25:30 -0600

    Delete warning added and fully functional

---
+++ wishlist/travel_wishlist/static/js/confirm_delete.js
@@ -0,0 +1,19 @@
+var deleteButtons = document.querySelectorAll('.delete');
+
+deleteButtons.forEach(function(button){
+
+  button.addEventListener('click', function(ev){
+
+    // Show a confirm dialog
+    var okToDelete = confirm("Delete place - are you sure?");
+
+    // If user presses no, prevent the form submit
+    if (!okToDelete) {
+      ev.preventDefault();  // Prevent the click event propagating
+    }
+
+    // Otherwise, the web page will continue processing the event, 
+    // and send the delete request to the server.
+
+  })
+});
